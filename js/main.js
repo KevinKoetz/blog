5c0855b890bd1a4bc946ac96847d2b82b5fdff1a
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-07-10 20:45:20 +0200

    Implement Basic Linking

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 const site = {
   root: null,
+  main: null,
   posts: null,
 
-  init: async function () {
-    this.root = window.location.href;
+  init: async function (val) {
+    this.root = window.location.origin;
+
+    //Register Event Listener for clicks
+    document.body.addEventListener("click", this.clickHandler.bind(this));
+
+    //Save main element for later
+    this.main = document.getElementsByTagName("main")[0];
 
     //Get all Post Names
     const response = await this.getFileFromServer("/db/posts.json");
     this.posts = JSON.parse(response)["names"];
 
     //Load initial Posts
-
-    this.loadPosts(this.posts.slice(0,2));
+    if (window.location.hash === "" || window.location.hash === "#/") {
+      this.loadContent(window.location.hash);
+    } else {
+      this.loadContent(window.location.hash);
+    }
   },
 
   /*TODO: Dont use Element.innerHTML */
@@ -22,20 +32,33 @@ const site = {
     }
     if (appendPosts) {
       const postContents = await Promise.all(responses);
-      const main = document.getElementsByTagName("main")[0];
       let combinedPostContent = "";
       for (const postContent of postContents) {
         combinedPostContent += postContent;
       }
-      main.innerHTML += combinedPostContent;
+      this.main.innerHTML += combinedPostContent;
     } else {
       const postContents = await Promise.all(responses);
-      const main = document.getElementsByTagName("main")[0];
       let combinedPostContent = "";
       for (const postContent of postContents) {
         combinedPostContent += postContent;
       }
-      main.innerHTML = combinedPostContent;
+      this.main.innerHTML = combinedPostContent;
+    }
+  },
+
+  loadContent: async function (hash) {
+    const path = hash.replace(/^#/, "");
+    if (path === "/" || path === "" || path === "./") {
+      this.loadPosts(this.posts.slice(0, 2));
+    } else {
+      main.innerHTML = await this.getFileFromServer(path);
+    }
+  },
+
+  clickHandler: function (event) {
+    if (event.srcElement.tagName === "A") {
+      this.loadContent(event.srcElement.hash);
     }
   },
 
@@ -43,23 +66,22 @@ const site = {
     /*TODO: Implement relative Paths like "./file" or "../folder/file" */
     /*TODO: Status Code handling*/
     let url = null;
-    if(path.startsWith("/")) url = path.replace("/", this.root);
+    if (path.startsWith("/")) url = this.root + path;
+    if (path.startsWith("./")) url = this.root + window.location.pathname + path.replace(/^\//, "");
     return await new Promise((resolve, reject) => {
       const httpRequest = new XMLHttpRequest();
       httpRequest.onreadystatechange = () => {
-        if(httpRequest.readyState === XMLHttpRequest.DONE){
-          if(httpRequest.status === 200){
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          if (httpRequest.status === 200) {
             resolve(httpRequest.responseText);
-          }else {
-            reject(`HTTP-status: ${httpRequest.status}`)
+          } else {
+            reject(`HTTP-status: ${httpRequest.status}`);
           }
         }
-      }
-      httpRequest.open("GET",url);
+      };
+      httpRequest.open("GET", url);
       httpRequest.send();
-    })
-
-    
+    });
   },
 
   getPostNames: async function () {
@@ -68,4 +90,4 @@ const site = {
   },
 };
 
-window.addEventListener("onload", site.init());
+window.addEventListener("load", site.init.bind(site));
